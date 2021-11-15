3ec2c7df42322beca91ebc543dec3f94f90427b5
JustynBougeno <93269509+JustynBougeno@users.noreply.github.com>
2021-11-15 09:47:44 -0600

    made changes to scrit.js and helper.js

--- script.js
+++ script.js
@@ -2,15 +2,57 @@
 
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by 
+    // // calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function(result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function() {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper
+    //     // functions to pick a planet fom the list of planets
+    //     // and add that information to your destination.
+    // })
+    // represented the form HTML element in our js file as an object
+    let form = document.querySelector("form");
+
+    form.addEventListener("submit", function(event) {
+        let faultyItems = document.getElementById("faultyItems")
+        let pilotName = document.getElementById("pilotName").value;
+        let copilotName = document.getElementById("copilotName").value;
+        let fuelLevel = document.getElementById("fuelLevel").value;
+        let cargoLevel = document.getElementById("cargoLevel").value;
+        // all code in here will only run after we submit the form
+        // are any of the fields empty?
+        // do all files have valid data inputted?
+        // formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoLevel, event);
+        if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+            alert("All fields required!");
+            event.preventDefault()
+        } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
+            alert("Pilot and Copilot must be valid names");
+            event.preventDefault()
+        } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+            alert("Cargo and fuel must be a number");
+            event.preventDefault()
+        }
+    
+        document.getElementById("pilotStatus").innerHTML += `: ${pilot}`;
+        document.getElementById("copilotStatus").innerHTML += `: ${copilot}`;
+        if (parseInt(fuelLevel) < 10000 || parseInt(cargoLevel) > 10000) {
+            document.getElementById("faultyItems").style.visibility = "visible";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
+            document.getElementById("launchStatus").style.color = "red";
+        } else {
+            document.getElementById("faultyItems").style.visibility = "hidden";
+            document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch!";
+            document.getElementById("launchStatus").style.color = "green";
+        }
+        // we are submitting this alert after we click submit
+        alert("Form submitted");
+        // then we preventDefault to make sure the page doesn't refresh (I know it's annoying)
+        event.preventDefault();
+    })
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,42 +2,77 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-   
+    let digitsInString = testInput.match(/[\d]/g);
+    if (!testInput || testInput === null || testInput.length === 0) {
+
+        return "Empty";
+    } else
+    if (typeof testInput === "string" && digitsInString.length === 0) {
+        return "Not a Number";
+    } else if (!isNaN(testInput)) {
+        return "Is a Number";
+    };
+
+
+    // validateInput(string);
+    // return "Empty", "Not a Number", or "Is a Number";
+
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel, event) {
+    console.log(list);
+    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+        alert("All fields required!");
+        event.preventDefault()
+    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
+        alert("Pilot and Copilot must be valid names");
+        event.preventDefault()
+    } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+        alert("Cargo and fuel must be a number");
+        event.preventDefault()
+    }
+
+    document.getElementById("pilotStatus").innerHTML += `: ${pilot}`;
+    document.getElementById("copilotStatus").innerHTML += `: ${copilot}`;
+    if (parseInt(fuelLevel) < 10000 || parseInt(cargoLevel) > 10000) {
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
+        document.getElementById("launchStatus").style.color = "red";
+    } else {
+        document.getElementById("faultyItems").style.visibility = "hidden";
+        document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch!";
+        document.getElementById("launchStatus").style.color = "green";
+    }
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function(response) {});
 
     return planetsReturned;
 }
 
-function pickPlanet(planets) {
-}
+function pickPlanet(planets) {}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
