68e953786793842108a9a30b3b1805ec8db0a326
JustynBougeno <93269509+JustynBougeno@users.noreply.github.com>
2021-11-15 16:05:26 -0600

    Changed a lot and finished my last project in unit 1 You rock Justyn

--- script.js
+++ script.js
@@ -2,57 +2,52 @@
 
 window.addEventListener("load", function() {
 
-    // let listedPlanets;
-    // // Set listedPlanetsResponse equal to the value returned by 
-    // // calling myFetch()
-    // let listedPlanetsResponse;
-    // listedPlanetsResponse.then(function(result) {
-    //     listedPlanets = result;
-    //     console.log(listedPlanets);
-    // }).then(function() {
-    //     console.log(listedPlanets);
-    //     // Below this comment call the appropriate helper
-    //     // functions to pick a planet fom the list of planets
-    //     // and add that information to your destination.
-    // })
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by 
+    // calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function(result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function() {
+        console.log(listedPlanets);
+        // listedPlanets is a listed array of objects with planet data.
+        // Below this comment call the appropriate helper
+        // functions to pick a planet fom the list of planets
+        // and add that information to your destination.
+       let randomPlanet = pickPlanet(listedPlanets);
+       console.log(randomPlanet.name);
+       let missionTarget = document.getElementById("missionTarget")
+       console.log(missionTarget);
+       missionTarget.innerHTML = `<h2>Mission Destination</h2>
+       <ol>
+           <li>Name: ${randomPlanet.name} </li>
+           <li>Diameter: ${randomPlanet.diameter} </li>
+           <li>Star: ${randomPlanet.star}</li>
+           <li>Distance from Earth: ${randomPlanet.distance} </li>
+           <li>Number of Moons: ${randomPlanet.moons} </li>
+       </ol>
+       <img src="${randomPlanet.image}">`
+    })
     // represented the form HTML element in our js file as an object
     let form = document.querySelector("form");
 
     form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
         let faultyItems = document.getElementById("faultyItems")
-        let pilotName = document.getElementById("pilotName").value;
-        let copilotName = document.getElementById("copilotName").value;
-        let fuelLevel = document.getElementById("fuelLevel").value;
-        let cargoLevel = document.getElementById("cargoLevel").value;
+        let pilotName = document.querySelector("input[name='pilotName']").value;
+        let copilotName = document.querySelector("input[name='copilotName']").value;
+        let fuelLevel = document.querySelector("input[name='fuelLevel']").value;
+        let cargoLevel = document.querySelector("input[name='cargoMass']").value;
         // all code in here will only run after we submit the form
         // are any of the fields empty?
         // do all files have valid data inputted?
-        // formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoLevel, event);
-        if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
-            alert("All fields required!");
-            event.preventDefault()
-        } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
-            alert("Pilot and Copilot must be valid names");
-            event.preventDefault()
-        } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
-            alert("Cargo and fuel must be a number");
-            event.preventDefault()
-        }
-    
-        document.getElementById("pilotStatus").innerHTML += `: ${pilot}`;
-        document.getElementById("copilotStatus").innerHTML += `: ${copilot}`;
-        if (parseInt(fuelLevel) < 10000 || parseInt(cargoLevel) > 10000) {
-            document.getElementById("faultyItems").style.visibility = "visible";
-            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
-            document.getElementById("launchStatus").style.color = "red";
-        } else {
-            document.getElementById("faultyItems").style.visibility = "hidden";
-            document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch!";
-            document.getElementById("launchStatus").style.color = "green";
-        }
+        formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoLevel, event);
+       
         // we are submitting this alert after we click submit
-        alert("Form submitted");
+        // alert("Form submitted");
         // then we preventDefault to make sure the page doesn't refresh (I know it's annoying)
-        event.preventDefault();
     })
 });
+
--- scriptHelper.js
+++ scriptHelper.js
@@ -18,12 +18,12 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-    let digitsInString = testInput.match(/[\d]/g);
+    // let digitsInString = testInput.match(/[\d]/g);
     if (!testInput || testInput === null || testInput.length === 0) {
 
         return "Empty";
     } else
-    if (typeof testInput === "string" && digitsInString.length === 0) {
+    if (isNaN(testInput)) {
         return "Not a Number";
     } else if (!isNaN(testInput)) {
         return "Is a Number";
@@ -35,41 +35,84 @@ function validateInput(testInput) {
 
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel, event) {
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     console.log(list);
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
         alert("All fields required!");
-        event.preventDefault()
+        // event.preventDefault()
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
         alert("Pilot and Copilot must be valid names");
-        event.preventDefault()
+        // event.preventDefault()
     } else if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
         alert("Cargo and fuel must be a number");
-        event.preventDefault()
-    }
-
-    document.getElementById("pilotStatus").innerHTML += `: ${pilot}`;
-    document.getElementById("copilotStatus").innerHTML += `: ${copilot}`;
-    if (parseInt(fuelLevel) < 10000 || parseInt(cargoLevel) > 10000) {
-        document.getElementById("faultyItems").style.visibility = "visible";
-        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
-        document.getElementById("launchStatus").style.color = "red";
+        // event.preventDefault()
     } else {
-        document.getElementById("faultyItems").style.visibility = "hidden";
-        document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch!";
-        document.getElementById("launchStatus").style.color = "green";
+        // wrapped this in else block so it doesn't run if inputs aren't valid
+        document.getElementById("pilotStatus").innerHTML = `Pilot Ready: ${pilot}`;
+        document.getElementById("copilotStatus").innerHTML = `Copilot Ready: ${copilot}`;
+        // right now we are only testing 1 case, and that is if fuelLevel is less than 10000
+        // OR cargoLevel is greater than 10000
+        // but there's many cases have to test
+
+        //done  if fuelLevel is less than 10000 AND cargoLevel is less than 10000
+        // not ready to launch, we need to update the fuelStatus and cargoStatus accordingly
+
+        // doneelse if fuelLevel AND cargoLevel are greater than 10000
+        // not ready to launch, fuelStatus fuel is good, cargoStatus not good
+
+        //this one is good to go
+        // else if fuelLevel is less than 10000 AND cargoLevel is greater than 10000
+        // not ready to launch, fuelStatus is not good, cargoStatus also not good
+
+        // done else if fuelLevel is greater than 10000 and cargoStatus is less than 10000
+        // ready for launch, fuelStatus is good and cargoStatus is also good
+        if (fuelLevel < 10000 && cargoLevel > 10000) {
+            list.style.visibility = "visible";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
+            document.getElementById("launchStatus").style.color = "red";
+            document.getElementById("fuelStatus").innerHTML = "Fuel too low to launch!";
+            document.getElementById("cargoStatus").innerHTML = "Cargo to heavy to launch!";
+        } else if (fuelLevel < 10000 && cargoLevel < 10000) {
+            list.style.visibility = "visible";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
+            document.getElementById("launchStatus").style.color = "red";
+            document.getElementById("fuelStatus").innerHTML = "Fuel too low to launch!";
+            document.getElementById("cargoStatus").innerHTML = "Cargo ready to launch!";
+        } else if (fuelLevel > 10000 && cargoLevel > 10000) {
+            list.style.visibility = "visible";
+            document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch!";
+            document.getElementById("launchStatus").style.color = "red";
+            document.getElementById("fuelStatus").innerHTML = "Fuel ready to launch!";
+            document.getElementById("cargoStatus").innerHTML = "Cargo not ready to launch!";
+        }  else if (fuelLevel > 10000 && cargoLevel < 10000) {
+            list.style.visibility = "visible";
+            document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch!";
+            document.getElementById("launchStatus").style.color = "green";
+            document.getElementById("fuelStatus").innerHTML = "Fuel ready to launch!";
+            document.getElementById("cargoStatus").innerHTML = "Cargo is ready to launch!";
+        }
     }
 }
-
+// My fetch created here already.
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then(function(response) {});
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+        return response.json()
+    });
 
     return planetsReturned;
 }
 
-function pickPlanet(planets) {}
+// this pickPlanet function will take an array of planet objects
+// select a random element from that array and return
+// we can use bracket notation to access indexes of an array
+// create a random index number, and then use it to select an element
+// from our array
+function pickPlanet(planets) {
+    let index = Math.floor(Math.random()*planets.length);
+    return planets[index];
+}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
